8fb1bdaaa802963c8ddd2fb093555eaa5e71c25f
Umair Khalid <umair.khalid@mckesson.com>
2017-07-07 19:01:52 -0500

    validation of input

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -52,6 +52,14 @@ namespace CityInfo.API.Controllers
                 return BadRequest();
             }
 
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name");
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
             if (city == null)
